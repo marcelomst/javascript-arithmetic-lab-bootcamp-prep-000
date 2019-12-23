ae5010aff29de5389dd0a9aeaa5c6fceb84c0e2a
Marcelo Martinez <marcelomst1@gmail.com>
2019-12-23 18:44:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function subtract(a,b) {
   return a-b
 }
 function multiply(a,b) {
-  return a-b
+  return a*b
 }
-function oint, we can fix the first four broken tests: we can define functions add(), subtract(), multiply(), (a,b) {
-  return a-b
+function divide (a,b) {
+  return a/b
 }
 function subtract(a,b) {
   return a-b
